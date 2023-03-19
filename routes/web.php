bc7d56e74c8d1545812e5e0aa3a60cc9aea55900
Diego Faria <diegoafbrito12@gmail.com>
2023-03-19 16:38:17 -0400

    ya inicia sesión

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
